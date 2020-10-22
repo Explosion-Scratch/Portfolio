46b9175682308826db779cb42a9de806387f95ff
--Explosion-- <explosion.scratch@gmail.com>
2020-10-22 08:05:54 -0500

    Scrollbar at the top

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,15 @@ function darkMode() {
   var dark = element.classList.toggle("dark-mode");
   window.localStorage.setItem("dark_mode", dark);
 }
+
+window.onscroll = function () {
+  scroll();
+};
+
+function scroll() {
+  var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+  var totalScroll =
+    document.body.scrollHeight - document.documentElement.clientHeight;
+  document.getElementById("scroll").style.width =
+    (scrollTop / totalScroll) * 100 + "%";
+}
