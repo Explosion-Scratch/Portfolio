c33a9a7c8e2ac2674aa08c273c85b9e9fde44910
--Explosion-- <explosion.scratch@gmail.com>
2020-10-26 07:25:20 -0500

    Yay!

--- scripts/main.js
+++ scripts/main.js
@@ -35,3 +35,23 @@ function scroll() {
   document.getElementById("scroll").style.width =
     (scrollTop / totalScroll) * 100 + "%";
 }
+
+function insertcss(css) {
+  var output = document.createElement("LINK");
+  output.setAttribute("rel", "stylesheet");
+  output.setAttribute("href", css);
+  document.getElementsByTagName("HEAD")[0].appendChild(output);
+}
+function insertjs(js) {
+  var output = document.createElement("SCRIPT");
+  output.setAttribute("src", js);
+  document.getElementsByTagName("HEAD")[0].appendChild(output);
+}
+window.onload = function () {
+  insertcss("/styles/main.css");
+  insertjs("https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js");
+  insertcss("/styles/images.css");
+  insertjs("/scripts/images.js");
+  insertcss("/styles/tooltips.css");
+  insertjs("https://code.iconify.design/1/1.0.7/iconify.min.js");
+};
